4b8922043d071811951e088e8d6498dc7de12f0d
Shubham Garg <39940156+shubhgsgarg@users.noreply.github.com>
2020-08-29 15:06:13 +0530

    Add files via upload

--- IpoApi/IpoApi/src/main/java/repository/IpoRepo.java
+++ IpoApi/IpoApi/src/main/java/repository/IpoRepo.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import ipo.Ipo;
+import ipoentity.Ipo;
 
 @Repository
 public interface IpoRepo extends JpaRepository<Ipo,Long> {
